b8b5da727f81c59abf277d709b2a3ba3102651d0
Brenan Kelley <brenankly@gmail.com>
2026-03-20 10:56:41 -0700

    test(go): add copy character codes coverage

---
+++ go/tests/copy_character_codes_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"testing"
+
+	vbml "github.com/Vestaboard/vbml/go"
+)
+
+func TestCopyCharacterCodes(t *testing.T) {
+	original := vbml.Board{{1, 2}}
+	copied := vbml.CopyCharacterCodes(original)
+
+	if len(copied) != 1 || len(copied[0]) != 2 {
+		t.Fatalf("unexpected copy shape: %#v", copied)
+	}
+	if copied[0][0] != 1 || copied[0][1] != 2 {
+		t.Fatalf("unexpected copy contents: %#v", copied)
+	}
+
+	original[0][0] = 3
+	if copied[0][0] != 1 {
+		t.Fatalf("copy should not share backing data, got %d", copied[0][0])
+	}
+}
